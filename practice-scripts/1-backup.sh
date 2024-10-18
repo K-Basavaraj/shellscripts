7a6c55b82cb9a4d2e2e9beec4144c4732248c65d
K-Basavaraj <kuruvabasavaraj09@gmail.com>
2024-10-18 15:40:46 +0530

    added prac1script

--- practice-scripts/1-backup.sh
+++ practice-scripts/1-backup.sh
@@ -8,7 +8,8 @@ Y="\e[33m"
 # Get User Input: Source directory, Destination directory, Number of days (optional)
 SOURCE_DIR=$1
 DES_DIR=$2
-NUM_DAYS=${3:-14} # Defaults to 14 days if not specified
+NUM_DAYS=${3:-14}                    # Defaults to 14 days if not specified
+TIMESTAMP=$(date +%Y-%m-%d-%H-%M-%S) # creating time stamp
 
 # Function to display usage instructions
 USAGE() {
@@ -37,8 +38,28 @@ FILES=$(find ${SOURCE_DIR} -name "*.log" -mtime +$NUM_DAYS)
 echo -e "$Y Files: $FILES $N" # Output the found files
 
 # Check if any files were found
-if [ ! -z "$FILES" ]; then # here -z bydefult its true if Files is empty, ! makes it false
+if [ ! -z "$FILES" ]; then          # here -z bydefult its true if Files is empty, ! makes it false
     echo -e "$G Files are found $N" # Inform if no old files were found
+
+    # Create a zip file in the destination directory with the current timestamp
+    ZIP_FILE="$DES_DIR/app-logs-$TIMESTAMP.zip" #zip file strcuture /home/ec2-user/<dest_dir>/<sor_dir>-<time-stamp>.zip
+    # Zip the found log files
+    find ${SOURCE_DIR} -name "*.log" -mtime +$NUM_DAYS | zip "$ZIP_FILE" -@
+
+    # Check if the zip file was created successfully
+    if [ -f $ZIP_FILE ]; then
+        echo -e "$G Successfully zippped files older than $NUM_DAYS $N"
+        # Remove the original files after zipping
+        # delete the files which are old one by one for that we using while loop
+        while IFS= read -r file; do
+            echo "Deleting file: $file"
+            rm -rf $file
+        done <<<$FILES
+    else
+        echo "Zipping the files is failed"
+        exit 1
+    fi
+
 else
     echo -e "$R No files older than $NUM_DAYS $N"
 fi
