07930407ff81cf6389d9a3547aa2c37a277d5421
K-Basavaraj <kuruvabasavaraj09@gmail.com>
2024-10-21 10:35:29 +0530

    added prac1script

--- practice-scripts/2-diskusage.sh
+++ practice-scripts/2-diskusage.sh
@@ -1,4 +1,8 @@
 #!/bin/bash
+R="\e[31m"
+G="\e[32m"
+N="\e[0m"
+Y="\e[33m"
 
 DISK_USAGE=$(df -hT | grep xfs)
 read -p "enter the threshold value to check health: " DISK_THRESHOLD
@@ -7,6 +11,6 @@ while IFS= read -r line; do #used in shell scripts to read lines from a file or 
     USAGE=$(echo $line | awk -F " " '{print $6F}' | cut -d "%" -f1)
     PARTITION=$(echo $line | grep xfs | awk -F " " '{print $NF}')
     if [ $USAGE -ge $DISK_THRESHOLD ]; then
-        echo "$PARTITION is more than $DISK_THRESHOLD, current value: $USAGE. Please check"
+        echo -e "$R $PARTITION is more than $DISK_THRESHOLD $N, $Y current value: $USAGE. Please check $N"
     fi
 done <<<$DISK_USAGE
