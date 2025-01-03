b08edea96590fc81d68b1085bf68b11400fab083
K-Basavaraj <kuruvabasavaraj09@gmail.com>
2025-01-03 21:55:20 +0530

    commit2

--- Level2_oper_shell_scripts/05_file_test_operator.sh
+++ Level2_oper_shell_scripts/05_file_test_operator.sh
@@ -10,3 +10,16 @@ folder=$2
 
 #Check if a File is Readable
 [ -r $file ] && echo "File is readable" || echo "File is not readable"
+
+: '
+ sh 05_file_test_operator.sh /home/ec2-user/myfile.txt /home/ec2-user/myfolder
+File exists
+Folder exists
+File is readable
+
+Note: 
+-x, -w This operator check whether the given file has execute access, write access or not. 
+If it has execute access, write access then it returns true otherwise false.
+-s operator: This operator checks the size of the given file. 
+If the size of given file is greater than 0 then it returns true otherwise it is false.
+'
