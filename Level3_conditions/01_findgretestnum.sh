ecf90d0a944d437149cd290c759b0d1651961dfa
K-Basavaraj <kuruvabasavaraj09@gmail.com>
2025-01-04 11:47:11 +0530

    commit4

---
+++ Level3_conditions/01_findgretestnum.sh
@@ -0,0 +1,30 @@
+#!/bin/bash
+
+read -p "enter the value of a: " a
+read -p "enter the value of b: " b
+
+if [ $a -gt $b ]; then
+    echo "$a is greatest in the given number"
+    else
+    echo "$b is is greatest in the given number"
+fi
+
+: '
+output:
+enter the value of a: 10
+enter the value of b: 50
+50 is is greatest in the given number
+
+enter the value of a: 9
+enter the value of b: 5
+9 is greatest in the given number
+'
+
+# read -p "Enter the FirstNumber: " Number1
+# read -p  "Enter the SecondNumber: " Number2
+
+# if [ $Number1 -gt $Number2 ]; then
+#     echo "Number:$Number1 is greter than $Number2"
+# else
+#     echo "Number:$Number1 is less than $Number2"
+# fi
---
+++ Level3_conditions/02_filefolderexitornot.sh
@@ -0,0 +1,23 @@
+#!/bin/bash
+
+# Get User Input: Source directory, Destination directory
+DIRECTORY=$1
+FILE=$2
+
+#2)Validate Input: if the argument is less than 2 it will exit with usage message function.
+#if you give include 2 and above 3 Not an issue. $# is How many variables/args passed to the script:
+if [ $# -lt 2 ]; then
+    exit 1
+fi
+
+if [ -d $DIRECTORY]; then
+    echo -e "$DIRECTORY Exist.."
+else
+    echo -e "$DIRECTORY does not exist...Please check"
+fi
+
+if [ -d $FILE]; then
+    echo -e "$FILE Exist..."
+else
+    echo "$FILE does not exist...Please check"
+fi
