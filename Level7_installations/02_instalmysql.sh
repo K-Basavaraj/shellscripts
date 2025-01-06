aaa2d75e304759d08166b20ede88128374b7f72a
K-Basavaraj <kuruvabasavaraj09@gmail.com>
2025-01-06 22:49:49 +0530

    6-1-commit3

--- Level7_installations/02_instalmysql.sh
+++ Level7_installations/02_instalmysql.sh
@@ -1,4 +1,13 @@
 #!/bin/bash
+: '
+install mysql through shell script
+ 1. check the user has root access or not
+ 2. if root access, proceed with the script
+ 3. otherwise through the error
+ 4. check already installed or not, if installed tell the user it is already insalled
+ 5. if not installed, install it
+ 6. check it is success or not
+'
 
 USERID=$(id -u)
 
@@ -22,3 +31,21 @@ if [ $? -ne 0 ]; then
 else
     echo "mysql is already installed nothing to do.."
 fi
+
+: '
+running the script: sudo sh 02_installmysql.sh 
+output: 
+Error: No matching Packages to list
+mysql is not installed going to install..
+Last metadata expiration check: 0:29:52 ago on Mon Jan  6 16:46:11 2025...installing..
+Complete!
+mysql is installed sucessfully..
+
+running the script 2nd time: sudo sh 02_installmysql.sh
+output: 
+Installed Packages
+mysql.x86_64      8.0.36-1.el9_3                                                     @rhel-9-appstream-rhui-rpms
+mysql is already installed nothing to do..
+'
+
+
