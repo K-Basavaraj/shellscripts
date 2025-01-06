7fe980c4d3d8de8c830d0c3b2bfa8d55d8b701f6
K-Basavaraj <kuruvabasavaraj09@gmail.com>
2025-01-06 22:22:33 +0530

    6-1-commit3

--- Level7_installations/01_install_git.sh
+++ Level7_installations/01_install_git.sh
@@ -15,7 +15,30 @@ always Root access having 0  example: [ root@ip-172-31-85-87 ~ ]# id #output uid
 [ ec2-user@ip-172-31-85-87 shellscripts ]$ id #output: uid=1001(ec2-user) gid=1001(ec2-user) groups=1001(ec2-user)
 '
 
-#!/bin/bash 
+#!/bin/bash
 
 USERID=$(id -u)
-dnf list installed git
+#echo "userID is: $USERID "
+#sh installmysql.sh         output: userID is: 1001 if you run this script as normal the uesr id is sum number but
+#sudo sh installmysql.sh    output: userID is: 0 If you run this script with sudo <script_name> it will run with root privileges.
+
+if [ $USERID -ne 0]; then
+    echo "please run the script with root privilages"
+    exit 1 #here user given 1 so shell undertand that its a filure. it underdatnd that have to come out of the progrm
+fi
+#Note: when userid is 1 not equal to 0 so, its true then it gives messege and exit it must be 0 equal to 0 to go to next.
+
+#4. check already installed or not, if installed tell the user it is already
+dnf install git -y
+if [ $? -ne 0 ]; then
+    echo "Git is not installed, going to install it.."
+    dnf install git -y
+    if [ $? -ne 0 ]; then
+        echo "Git installation is not sucess.. checkit."
+        exit 1
+    else
+        echo "Git installation is sucessfull.."
+    fi
+else
+    echo "Git is alrwady installed, nothing to do.."
+fi
