6a808dd426a516c526b665d0fd7842587fb7f013
K-Basavaraj <kuruvabasavaraj09@gmail.com>
2025-01-08 12:41:41 +0530

    8-1commit1

---
+++ Level7_installations/03_leverage_code_condition_install.sh
@@ -0,0 +1,44 @@
+#!/bin/bash
+
+USER_ID=$((id - u))
+
+: '
+you calso keep this checking root user in a function also 
+CHECK_ROOT() {
+if [ $USER_ID -ne 0 ]; then
+    echo "please run the script with root privilages"
+    exit 1
+fi
+}
+'
+
+validate() {
+    if [ $1 -ne 0 ]; then
+        echo "$2 is FAILED.. checkit."
+        exit 1
+    else
+        echo "$2 is SUCESSFULL.."
+    fi
+}
+
+dnf list installed git
+validate $? "Listing Git"
+
+if [ $? -ne 0 ]; then
+    echo "Git is not installed, going to install it.."
+    dnf install git -y
+    validate $? "Installing Git"  # here i am calling validate function insted of Nested block in the condition concept
+else
+    echo "Git is already installed nothing to do.."
+fi
+
+dnf list installed mysql
+validate $? "Listing mysql"
+
+if [ $? -ne 0 ]; then
+    echo "Mysql is not installed, going to install it.."
+    dnf install mysql -y
+    validate $? "Installing mysql"    # here i am calling validate function insted of Nested block in the condition concept
+else
+    echo "Mysql is already installed nothing to do.."
+fi
---
+++ Level7_installations/05_modify_04_installion_script.sh
@@ -0,0 +1,35 @@
+#!/bin/bash
+#we need use -e when colors are used
+R="\e[31m"
+G="\e[32m"
+N="\e[0m"
+
+USER_ID=$((id - u))
+
+validate() {
+    if [ $1 -ne 0 ]; then
+        echo "$2 is FAILED.. checkit."
+        exit 1
+    else
+        echo "$2 is SUCESSFULL.."
+    fi
+}
+
+validate_installation() {
+    if [ $1 -ne 0 ]; then
+        echo -e "$R $2 is not installed..$N  $Y going to install it..$N"
+    else
+        echo -e "$G $2 is already installed nothing to do.."
+    fi
+
+}
+
+for package in $@; do
+    dnf list installed $package
+    validate_installation $? "$package"
+
+    dnf install $package -y
+    validate $? "Installing $package"
+done
+
+
