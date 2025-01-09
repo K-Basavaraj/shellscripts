ab7b8d1b1d1c360c6d313aa1cc63f8b09598c8c7
K-Basavaraj <kuruvabasavaraj09@gmail.com>
2025-01-09 07:59:51 +0530

    8-1commit1

--- Level7_redirectors/01_basic_redirectors.sh
+++ Level7_redirectors/01_basic_redirectors.sh
@@ -6,19 +6,3 @@
 &--> for both sucess and failure.
 '
 
-mkdir -p /home/ec2-user/test home/ec2-user/dev 
-echo "Hello how are you"
-
-ls -l /home/ec2-user/
-
-: '
-Here output prints on the termianl.
-output: 
-[ ec2-user@ip-172-31-80-84 ~/practice/shellscripts/Level7_redirectors ]$ sh 01_basic_redirectors.sh
-Hello how are you
-total 0
-drwxr-xr-x 3 ec2-user ec2-user 26 Jan  9 02:14 practice
-drwxr-xr-x 2 ec2-user ec2-user  6 Jan  9 02:21 test
-'
-
-ls -l /home/ec2-user/  > /home/ec2-user/output.txt
