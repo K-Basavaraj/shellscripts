806e02a3582584f1f8b6ec88671ef339c45bdb4e
K-Basavaraj <kuruvabasavaraj09@gmail.com>
2025-01-09 07:52:25 +0530

    8-1commit1

--- Level7_redirectors/01_basic_redirectors.sh
+++ Level7_redirectors/01_basic_redirectors.sh
@@ -6,7 +6,7 @@
 &--> for both sucess and failure.
 '
 
-mkdir -p /home/ec2-user/test #/home/ec2-user/dev 
-# echo "Hello how are you"
+mkdir -p /home/ec2-user/test home/ec2-user/dev 
+echo "Hello how are you"
 
-# ls -l /home/ec2-user/
+ls -l /home/ec2-user/
