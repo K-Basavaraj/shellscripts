91095f6506e234d29239fab3aa9f4df3c0065fe8
K-Basavaraj <kuruvabasavaraj09@gmail.com>
2025-01-09 09:29:15 +0530

    8-1commit1

--- Level7_redirectors/01_basic_redirectors.sh
+++ Level7_redirectors/01_basic_redirectors.sh
@@ -11,3 +11,17 @@ mkdir -p /home/ec2-user/dev /home/ec2-user/test
 touch /home/ec2-user/output.txt
 
 ls -l /home/ec2-user 1> /home/ec2-user/output.txt
+
+: '
+Here output of ls command will save on the output.txt insted of terminal
+output: 
+cat /home/ec2-user/output.txt
+total 4
+-rw-r--r-- 1 ec2-user ec2-user 123 Jan  9 03:54 details.txt
+drwxr-xr-x 2 ec2-user ec2-user   6 Jan  9 03:56 dev
+-rw-r--r-- 1 ec2-user ec2-user   0 Jan  9 03:56 output.txt
+drwxr-xr-x 3 ec2-user ec2-user  26 Jan  9 03:52 practice
+drwxr-xr-x 2 ec2-user ec2-user   6 Jan  9 03:56 test
+'
+
+llls -l /home/ec2-user 1> /home/ec2-user/output.txt # here ls command is wrong 
