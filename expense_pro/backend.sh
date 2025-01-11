2e38f512c9336be0ecbe5f30c67788e46e1d46e3
K-Basavaraj <kuruvabasavaraj09@gmail.com>
2025-01-11 13:50:05 +0530

    9-1commit1

---
+++ expense_pro/backend.sh
@@ -0,0 +1,46 @@
+#!/bin/bash
+R="\e[31m"
+G="\e[32m"
+Y="\e[33m"
+N="\e[0m"
+
+# Create a file structure /var/log/shell-script/installredirecttologs-<timestamp>.log
+LOG_FOLDER="/var/log/expense-pro"
+mkdir -p $LOG_FOLDER
+
+SCRIPT_NAME=$(echo $0 | cut -d "." -f1)
+TIMESTAMP=$(date +%Y-%m-%d-%H-%M-%S)
+
+LOG_FILE="$LOG_FOLDER/$SCRIPT_NAME-$TIMESTAMP.log"
+
+#Root user permission
+USER_ID=$(id -u)
+CHECK_ROOT() {
+    if [ $USER_ID -ne 0 ]; then
+        echo -e "$R PLEASE RUN THE SCRIPT WITH ROOT PRIVIEGES..$N $Y eg: sudo <script_name>$N" | tee -a $LOG_FILE
+        exit 1
+    fi
+}
+
+echo -e "$Y script started executing at:$N $G $(date)$N" | tee -a $LOG_FILE
+
+VALIDATE() {
+    if [ $1 -ne 0 ]; then
+        echo -e "$R $2 is FAILLED.. PLEASE CHECKIT..$N" | tee -a $LOG_FILE
+        exit 1 
+    else
+        echo -e "$G $2 is SUCESSFULL...$N" | tee -a $LOG_FILE
+    fi
+}
+
+dnf module disable nodejs -y
+VALIDATE $? "Disable defult nodesjs" &>> $LOG_FILE
+
+
+dnf module enable nodejs -y
+VALIDATE $? "Enabled nodejs:22" &>> $LOG_FILE
+
+dnf install nodejs -y 
+VALIDATE $? "installing nodejs" &>> $LOG_FILE
+
+useradd expense
