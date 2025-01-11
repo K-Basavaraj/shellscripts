ca62ce3ac52624463450b1b9952efb659730bc60
K-Basavaraj <kuruvabasavaraj09@gmail.com>
2025-01-11 19:11:13 +0530

    9-1commit1

---
+++ Level9_SystemMaintenance/01_delete_old_log_files.sh
@@ -0,0 +1,33 @@
+#/bin/bash
+R="\e[31m"
+G="\e[32m"
+N="\e[0m"
+
+#write a script to delete only .log files which are older than 14 days
+: '
+#1) finding .log files and days 
+find . -name "*.log"            # Find all .log files in the current directory and its subdirectories.
+find . -name "*.log" -mtime +14 # Find all .log files that have been modified more than 14 days ago.
+# Find all .log files that have been modified more than 14 days ago 
+# and list their details using ls -lrt. [ find . -name "*.log" -mtime +14 -exec ls -lrt {} \; ]
+
+#2) In which directory we need to delete?
+#3) Check is that Directory exist?
+#4) if yes, find the .log files and delete 
+#5) if no exit or print message.
+'
+# Function to check if the directory exists
+directory() {
+    if [ -d "$1" ]; then
+        echo -e "$G $1  Exists $N"
+    else
+        echo -e "$R $1 Does not Exist $N"
+        exit 1
+    fi
+}
+
+# Prompt for source directory input
+read -p "Please enter the source Directory: " SOURCE_DIR 
+
+# Check if the directory exists
+directory "$SOURCE_DIR"
