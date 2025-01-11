b3a64fe6eba392a57fe7f041d06909a5995c1107
K-Basavaraj <kuruvabasavaraj09@gmail.com>
2025-01-11 14:19:06 +0530

    9-1commit1

--- expense_pro/backend.sh
+++ expense_pro/backend.sh
@@ -59,5 +59,7 @@ VALIDATE $? "Downloading backend application code"
 
 cd /app
 rm -rf /app/* # remove the existing code
-unzip /tmp/backend.zip
+unzip /tmp/backend.zip &>>$LOG_FILE
 VALIDATE $? "Extracting backend application code"
+
+npm install &>>$LOG_FILE
