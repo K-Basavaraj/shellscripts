46576783667c97a8a46fddd5db962dacae089f98
K-Basavaraj <kuruvabasavaraj09@gmail.com>
2025-01-13 12:58:12 +0530

    9-1commit1

--- Level9_SystemMaintenance/05_disk_usage_using_read.sh
+++ Level9_SystemMaintenance/05_disk_usage_using_read.sh
@@ -2,3 +2,11 @@
 
 DISK_USAGE_DETAILS=$(df -hT)
 echo -e "this is the Result:\n '$DISK_USAGE_DETAILS'"
+
+read -p "Please Enter the type/field you need: " TYPE 
+
+#read -p "Please Enter the threshold value to check Health" DISK_THRESHOLD
+
+DISK_USAGE=$(echo "$DISK_USAGE_DETAILS"| grep $TYPE)
+
+echo -e "Filtered Result:\n$DISK_USAGE"
