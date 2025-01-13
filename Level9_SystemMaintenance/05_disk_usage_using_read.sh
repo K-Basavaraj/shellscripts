90b97093c803e2a8e3aa90fd888dbcf9401efd92
K-Basavaraj <kuruvabasavaraj09@gmail.com>
2025-01-13 13:37:16 +0530

    9-1commit1

--- Level9_SystemMaintenance/05_disk_usage_using_read.sh
+++ Level9_SystemMaintenance/05_disk_usage_using_read.sh
@@ -1,12 +1,16 @@
-#/bin/bash 
+#!/bin/bash
+R="\e[31m"
+G="\e[32m"
+N="\e[0m"
+Y="\e[33m"
 
-DISK_USAGE_DETAILS=$(df -hT)
-echo -e "this is the Result:\n '$DISK_USAGE_DETAILS'"
+DISK_USAGE=$(df -hT | grep xfs)
+read -p "enter the threshold value to check health: " DISK_THRESHOLD
 
-read -p "Please Enter the Type/field you need: " TYPE 
-
-#read -p "Please Enter the threshold value to check Health" DISK_THRESHOLD
-
-DISK_USAGE=$(echo "$DISK_USAGE_DETAILS"| grep $TYPE)
-
-# echo -e "Filtered Result:\n$DISK_USAGE"
+while IFS= read -r line; do #used in shell scripts to read lines from a file or standard input.
+    USAGE=$(echo $line | awk -F " " '{print $6F}' | cut -d "%" -f1)
+    PARTITION=$(echo $line | grep xfs | awk -F " " '{print $NF}')
+    if [ $USAGE -ge $DISK_THRESHOLD ]; then
+        echo -e "$R $PARTITION is more than $DISK_THRESHOLD $N, $Y current value: $USAGE. Please check $N"
+    fi
+done <<<$DISK_USAGE
