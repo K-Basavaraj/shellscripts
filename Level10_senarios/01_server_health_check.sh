ee0afaf91edeb2f46bde2cc5df76f8d7eee5e0d7
K-Basavaraj <kuruvabasavaraj09@gmail.com>
2025-02-11 07:22:13 +0530

    changes in script

--- Level10_senarios/01_server_health_check.sh
+++ Level10_senarios/01_server_health_check.sh
@@ -3,11 +3,12 @@
 echo "checking the server health"
 
 #check cpu usage 
+echo "CPU usage: "
 top -bn1 | grep "Cpu(s)" | awk '{print "used: " $2"%, Idle; " $8"%"}'
 
 #check memory usage 
 echo "Memory Usage: " 
-free -h | awk 'NR==2{print "Used: "$3 ", Free: $4}'
+free -h | awk 'NR==2{print "Used: "$3 ", Free: " $4}'
 
 #check disk usage 
 echo "Disk usage:"
